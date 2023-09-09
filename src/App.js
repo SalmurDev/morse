c3645887d76cc9874576c91fbd573b996f3cdbd0
SalmurDev <filippo.bortolo@outlook.com>
2023-09-09 12:17:14 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
   const [screenColor, setScreenColor] = useState('black')
-
+  const morse = new Map([
+    ['a', '.-'],
+    ['b', '-...'],
+    ['c', '-.-.'],
+    ['d', '-..'],
+    ['e', '.'],
+    ['f', '..-.'],
+    ['g', '--.'],
+    ['h', '....'],
+    ['i', '..'],
+    ['j', '.---'],
+    ['k', '-.-'],
+    ['l', '.-..'],
+    ['m', '--'],
+    ['n', '-.'],
+    ['o', '---'],
+    ['p', '.--.'],
+    ['q', '--.-'],
+    ['r', '.-.'],
+    ['s', '...'],
+    ['t', '-'],
+    ['u', '..-'],
+    ['v', '...-'],
+    ['w', '.--'],
+    ['x', '-..-'],
+    ['y', '-.--'],
+    ['z', '--..'],
+    ['0', '-----'],
+    ['1', '.----'],
+    ['2', '..---'],
+    ['3', '...--'],
+    ['4', '....-'],
+    ['5', '.....'],
+    ['6', '-....'],
+    ['7', '--...'],
+    ['8', '---..'],
+    ['9', '----.'],
+    [" ", "....."],
+  ])
   useEffect(() => {
     const onKeyDown = (e) => {
       if (e.key === ' ') {
@@ -11,8 +50,22 @@ function App() {
       }
     }
     const onKeyUp = (e) => {
-      if (e.key === ' ') {
+      let key = e.key
+      let phrases = ['1', '2']
+      console.log(e.key);
+      if (key === ' ') {
         setScreenColor('black');
+      } else if(phrases.includes(key)) {
+        console.log('entered');
+        try {
+          async function getMessage(key) {
+            let res = await axios.get(`http://localhost:8000/morses/${key}`)
+            let json = res.data            
+            return
+          }          
+        } catch (error) {
+          console.log(error);
+        }
       }
     }
     document.addEventListener("keydown", onKeyDown);
@@ -25,8 +78,6 @@ function App() {
 
   return (
     <div className={`App ${screenColor}`}>
-      <header className="App-header">
-      </header>
     </div>
   );
 }
